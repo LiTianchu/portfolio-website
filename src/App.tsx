3ad137a4de16b11de095962b2d7ce62eca3c0ce6
LiTianchu <tianchu0522@gmail.com>
2026-01-23 15:58:00 +0800

    add in tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { useState } from 'react';
 import type { FC } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 import { useAppSelector, useAppDispatch } from './hook';
-import { addTodo, deleteTodo } from './todoSlice';
+import { addTodo } from './todoSlice';
 
 const App: FC = () => {
-    const [count, setCount] = useState<number>(0);
     const [text, setText] = useState<string>('');
     const todos = useAppSelector((state) => state.todos); // retrieve a piece of state from the store
     const dispatch = useAppDispatch(); // get the dispatch function to send actions to the store
@@ -23,37 +19,14 @@ const App: FC = () => {
 
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank" rel="noreferrer">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-            <div>
+            <div className="bg-dark-ink">
                 <h1>Todo App</h1>
                 <input
                     type="text"
                     value={text}
                     onChange={(e) => setText(e.target.value)}
                     placeholder="Enter a todo"
+                    className=""
                 />
                 <button onClick={handleAddTodo}>Add Todo</button>
             </div>
